6ce839fd1437b19ab5ed75af83ab482b9bf13bc8
wathika-eng <wathika02@gmail.com>
2024-05-01 10:46:21 +0300

    states&&rendering

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function Item({ name, category }) {
-  return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
-  );
+	const [inCart, setinCart] = useState(true);
+	const [categoryClass, setCatClass] = useState(false);
+
+	const cartAdd = () => {
+		setinCart(!inCart);
+		setCatClass(!categoryClass);
+	};
+	return (
+		<li className={categoryClass ? 'in-cart' : ''}>
+			<span>{name}</span>
+			<span className='category'>{category}</span>
+			<button onClick={cartAdd} className={categoryClass ? 'remove' : 'add'}>
+				{inCart ? 'Add' : 'Remove from'} Cart
+			</button>
+		</li>
+	);
 }
 
 export default Item;
